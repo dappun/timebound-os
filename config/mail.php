3d1ea44ae584da82c1460716408b8238423379db
Rachel Jaro <rachel@koodi.ph>
2017-02-01 07:51:34 +0800

    Fix forgot password redirect and layout

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => env('SITE_EMAIL'), 'name' => env('SITE_EMAIL_NAME')],
 
     /*
     |--------------------------------------------------------------------------
