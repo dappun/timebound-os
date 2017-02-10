5ec38c2efbe14a11b715af73172df84726263b5f
Rachel Jaro <rachel@koodi.ph>
2017-02-11 07:30:21 +0800

    Move all resources to modules folder

--- modules/Core/Resources/views/layouts/master.blade.php
+++ modules/Core/Resources/views/layouts/master.blade.php
@@ -7,31 +7,41 @@
 
     <title>{{ env('SITE_NAME', '') }}</title>
 
-    <!-- Fonts -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet'
-          type='text/css'>
-    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
-
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-    <!-- DataTable Bootstrap -->
-    <link href="https://cdn.datatables.net/1.10.11/css/dataTables.bootstrap.min.css" rel="stylesheet">
-
-    <!-- Select2 -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.min.css" rel="stylesheet" />
-
-    <link href="{{ asset('css/sidebar.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
-
-    <link href="{{ asset('lib/smartmenus/addons/bootstrap/jquery.smartmenus.bootstrap.css') }}" rel="stylesheet">
-
+    <link rel='stylesheet' href="{{ asset('css/font-awesome/css/font-awesome.min.css') }}" type='text/css'>
+    <link rel="stylesheet" href="{{ asset('lib/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('lib/bootstrap/css/bootstrap-theme.min.css') }}">
+    
+    <script type="text/javascript" src="{{ asset('dist/main.js') }}"></script>
     <script type="text/javascript">
-        var apiURl = '{{ url('/') }}';
+        /* All core configurations from backend */
+        var core = {};
+        core.url = function(uri)
+        {
+            return '{{ url('/') }}' + uri;
+        }
+
+        core.userTimezone = '{!! userTimezone() !!}';
+
+        core.pad = function (d) {
+            return (d < 10) ? '0' + d.toString() : d.toString();
+        }
+
+        core.formatGMH = function(seconds) {
+            hours = Math.floor(seconds / 3600);
+            minutes = Math.floor(seconds / 60 % 60);
+            secs = Math.floor(seconds % 60);
+            
+            return core.pad(hours) + ':' + core.pad(minutes) + ':' + core.pad(secs);
+        }
+
+        core.setting = {
+            alert_notice: '{{ Config::get('timesheet.overtime.notice') }}',
+            alert_warning: '{{ Config::get('timesheet.overtime.warning') }}'
+        }
     </script>
 
-    
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
     @yield('style')
 </head>
 <body id="app-layout">
@@ -58,11 +68,6 @@
 
         @if (Auth::guest())
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
-            <!-- Left Side Of Navbar -->
-            <ul class="nav navbar-nav">
-                <li><a href="{{ url('/home') }}">Home</a></li>
-            </ul>
-
             <!-- Right Side Of Navbar -->
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="{{ url('/login') }}">Login</a></li>
@@ -73,25 +78,26 @@
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
             <!-- Left Side Of Navbar -->
             <ul class="nav navbar-nav">
-                @include('layouts.menu')
+                @include('core::layouts.menu')
             </ul>
 
-            <!-- Right Side Of Navbar -->
-            <div class="dropdown pull-right">
-                <a class="dropdown-toggle" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true"><img src="{{ url(\Auth::user()->profile_image) }}" class="img-circle special-img" style="height:25px;">
+            <div class="pull-right">
+                <ul class="nav navbar-nav">
+                <li class="{{ \Request::is('report*') ? 'active' : '' }}">
+                    <a class="dropdown-toggle" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                        <img src="{{ url(\Auth::user()->profile_image) }}" class="img-circle special-img" style="height:25px;">
                     <span class="name">{{\Auth::user()->name}}</span>
                     <span class="caret"></span>
-                </a>
+                    </a>
 
-                <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenu1">
-                    <li><a href="{{ url('/account') }}">My Account</a></li>
-                    <li><a href="{{ url('/logout') }}">Logout</a></li>
+                    <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenu1">
+                        <li><a href="{{ route('account.view') }}">My Account</a></li>
+                        <li><a href="{{ route('account.setting') }}">Settings</a></li>
+                        <li><a href="{{ url('/logout') }}">Logout</a></li>
+                    </ul>
+                </li>
                 </ul>
             </div>
-
-            <!-- <ul class="nav navbar-nav navbar-right">
-                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-            </ul> -->
         </div>
 
         @endif
@@ -100,42 +106,15 @@
 
 <!-- Page Content -->
 <div id="page-content-wrapper">
-
     <div class="container">
-
         <div class="row">
-            @if (config('infyom.laravel_generator.add_on.menu.enabled') and !Auth::guest())
-                <div class="col-md-10 col-md-offset-2">
-                    @yield('content')
-                </div>
-            @else
-                <div class="col-md-12">
-                    @yield('content')
-                </div>
-            @endif
+            <div class="col-md-12">
+                @yield('content')
+            </div>
         </div>
     </div>
 </div>
 <!-- /#page-content-wrapper -->
-
-<!-- JavaScripts -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-
-<!-- Bootstrap -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-<!-- Datatables -->
-<script src="https://cdn.datatables.net/1.10.11/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.10.11/js/dataTables.bootstrap.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.2.1/js/dataTables.buttons.min.js"></script>
-<script src="https://cdn.datatables.net/buttons/1.2.1/js/buttons.colVis.min.js"></script>
-
-<!-- Select2 -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js"></script>
-
-<script type="text/javascript" src="{{ asset('lib/smartmenus/jquery.smartmenus.js') }}" rel="stylesheet"></script>
-<script type="text/javascript" src="{{ asset('lib/smartmenus/addons/bootstrap/jquery.smartmenus.bootstrap.min.js') }}" rel="stylesheet"></script>
-
 
 @yield('scripts')
 
